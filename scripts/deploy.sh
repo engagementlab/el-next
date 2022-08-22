10bd8dcfc0d1298e0b09975b2fe6cd8d9e638361
Johnny Richardson <johnny.c.rich@gmail.com>
2022-08-22 16:50:30 -0400

    refactor: deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,12 @@
 # Source/load nvm
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh;
 
-cd client;
+
 nvm use;
 yarn;
+yarn build;
+cd apps/tngvi;
+yarn;
+yarn dev;
 
 pm2 restart 'transform-narratives'; 

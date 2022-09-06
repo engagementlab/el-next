dc596292740245c28bac2a930b93f9d1452e4411
Johnny Richardson <johnny.c.rich@gmail.com>
2022-09-06 18:13:11 -0400

    feat(function): new UserEditOrchestrator function.

---
+++ packages/functions/UserEditOrchestrator/index.ts
@@ -0,0 +1,12 @@
+﻿import * as df from 'durable-functions';
+
+const orchestrator = df.orchestrator(function* (context) {
+  const result = yield context.df.callActivity(
+    'PersonProfileEdit',
+    context.df.getInput()
+  );
+  context.log(result);
+  //   context.df.setCustomStatus(response.token);
+});
+
+export default orchestrator;

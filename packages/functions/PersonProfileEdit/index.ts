05b6557c9a9694df51482550468f75d14a0e23b9
Johnny Richardson <johnny.c.rich@gmail.com>
2022-09-06 18:12:39 -0400

    refactor(function): renames.

--- packages/functions/PersonProfileEdit/index.ts
+++ packages/functions/PersonProfileEdit/index.ts
@@ -1,6 +1,5 @@
 ﻿import { AzureFunction, Context } from '@azure/functions';
 
-const cuid = require('cuid');
 const { Client } = require('pg');
 
 const activityFunction: AzureFunction = async function (context: Context) {
@@ -22,17 +21,25 @@ const activityFunction: AzureFunction = async function (context: Context) {
     context.done(`Connect error: ${e.message}`);
   }
   try {
-    const text =
-      'UPDATE Person SET name = $1 title = $2 blurb = $3 remembrance = $4 WHERE id = $5';
+    const getBioIdText = 'SELECT "bioId" FROM "User" WHERE "accessToken" = $1';
+    const bioIdResult = await client.query(getBioIdText, [body.token]);
+    if (bioIdResult.rowCount === 0) {
+      context.done('User not found');
+      return;
+    }
+    context.log(bioIdResult);
+    const updateProfileText = `UPDATE "Person" SET "name" = $1 "title" = $2 "blurb" = $3 "remembrance" = $4 WHERE "id" = ${bioIdResult}`;
+    await client.query(updateProfileText, [context.bindingData.body.userId]);
     const values = [
-      cuid(),
       body.name,
       body.title,
       body.blurb,
       body.remembrance ? body.remembrance : '',
     ];
-    await client.query(text, values);
+    await client.query(updateProfileText, values);
     await client.end();
+    // const text =
+    //   'INSERT INTO "Person"(id, "name", "title", ) VALUES($1, $2, $3, $4, $5)';
   } catch (e) {
     context.log.error(`Query error: ${e.message}`);
     throw e;

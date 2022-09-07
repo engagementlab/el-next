72a5d597f26eb724c2a06fc2f462ca5c26288eb3
Johnny Richardson <johnny.c.rich@gmail.com>
2022-09-07 13:14:17 -0400

    feat(function): new PersonProfileGet function.

---
+++ packages/functions/PersonProfileGet/index.ts
@@ -0,0 +1,46 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+
+const { Client } = require('pg');
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  //   const body = req.query
+  if (!req.query.token) {
+    context.res = {
+      status: 500,
+      body: 'token',
+    };
+  }
+  const client = new Client({
+    connectionString: process.env.DB_URI,
+  });
+  try {
+    await client.connect();
+  } catch (e) {
+    context.log(e);
+    context.done(`Connect error: ${e.message}`);
+  }
+  try {
+    const getBioIdText = 'SELECT "bioId" FROM "User" WHERE "accessToken" = $1';
+    const bioIdResult = await client.query(getBioIdText, [req.query.token]);
+    if (bioIdResult.rowCount === 0) {
+      context.done('User not found');
+    }
+
+    const getProfileText = `SELECT "name", "title", "blurb", "remembrance" FROM "Person" WHERE "id" = '${bioIdResult.rows[0].bioId}'`;
+    const profileResult = await client.query(getProfileText);
+    await client.end();
+
+    context.res = {
+      status: 200,
+      body: profileResult.rows[0],
+    };
+  } catch (e) {
+    context.log.error(`Query error: ${e.message}`);
+    throw e;
+  }
+};
+
+export default httpTrigger;

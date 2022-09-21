d98a81f2e4ede89f817758881bcfaf7b6e45a9b9
Johnny Richardson <johnny.c.rich@gmail.com>
2022-09-21 12:20:53 -0400

    Revert "Revert "feat(tngvi): WIP community page filtering.""
    This reverts commit 31509529344ed8576a6c4579629e76eb3a1ad046.

--- apps/tngvi/pages/about/community.tsx
+++ apps/tngvi/pages/about/community.tsx
@@ -1,13 +1,25 @@
-import { ReactNode } from 'react';
 import { InferGetStaticPropsType } from 'next';
-import { BlockRenderers } from '@el-next/components/blockRenderers';
-import { DocumentRenderer, DocumentRendererProps } from '@keystone-6/document-renderer';
-
 import { Image } from '@el-next/components/image';
+
+import create, {
+  Mutate,
+  GetState,
+  SetState,
+  StoreApi,
+  UseBoundStore,
+  State
+} from 'zustand'
+import {
+  subscribeWithSelector
+} from 'zustand/middleware'
+import _ from 'lodash';
+import {
+  AnimatePresence
+} from "framer-motion"
+
 import Layout from '../../components/Layout';
-import { HeadingStyle } from '@el-next/components/headingStyle';
-import { DocRenderers } from '@el-next/components/docRenderers';
 import query from "../../apollo-client";
+import { useRouter } from 'next/router';
 
 type CommunityPage = {
     values: any;
@@ -15,6 +27,7 @@ type CommunityPage = {
 
 type Person = {
     name: string;
+    tag: string;
     title: string;
     remembrance: string;
     blurb: string;
@@ -22,33 +35,135 @@ type Person = {
     content: any;
 }; 
 
-const rendererOverrides = {
-    heading: (level: number, children: ReactNode, textAlign: any) => {
+interface ItemRendererProps<T> {
+  item: T;
+  toggleFilter: (filter: string) => void;
+}
+type FilterState = {
+  currentFilter: string;
+  toggle: (filter: string) => void
+  reset: () => void
+}
 
-      const customRenderers = {
-        4: 'text-xl font-semibold text-coated my-8',
-        5: 'text-lg font-extrabold text-purple'
-      };
-      return HeadingStyle(level, children, textAlign, customRenderers);
-  },
-};
+
+// Replicated from https://github.com/pmndrs/zustand/blob/a418fd748077c453efbff2d03641ce0af780b3c7/src/middleware/subscribeWithSelector.ts
+interface StoreSubscribeWithSelector < T extends State > {
+  subscribe: {
+      (listener: (selectedState: T, previousSelectedState: T) => void): () => void 
+          <U>(
+              selector: (state: T) => U,
+              listener: (selectedState: U, previousSelectedState: U) => void,
+              options ? : {
+                  equalityFn ? : (a: U, b: U) => boolean
+                  fireImmediately ? : boolean
+              }
+          ): () => void
+  }
+}
 
 export default function Community({ page, people }: InferGetStaticPropsType<typeof getStaticProps>) {
+
+
+  const router = useRouter();
+  const preSelectedFilter =  Object.keys(router.query).length === 1 ? Object.keys(router.query)[0] : '';
+  let selectedFilter =  preSelectedFilter.length > 0 ? preSelectedFilter : '';
+  console.log('preSelectedFilter',router.query)
+  
+  // Create store with Zustand
+  const useStore = create <
+  FilterState,
+  SetState < FilterState > ,
+  GetState < FilterState > ,
+  Mutate < StoreApi < FilterState > , [
+      ["zustand/subscribeWithSelector", never]
+  ] >
+  >
+  (
+      subscribeWithSelector((set) => ({
+        currentFilter: selectedFilter,
+        toggle: (filter: string) => set({
+            currentFilter: filter
+        }),
+        reset: () => set({
+            currentFilter: ''
+        }),
+  })));
+useStore.subscribe(state => state.currentFilter, (current) => {
+  console.log(current)
+  history.replaceState({}, 'Filtered Data', `${location.pathname}?${current}`);
+});    
+  // Store get/set
+  selectedFilter = useStore(state => state.currentFilter);
+  const haveFilters = selectedFilter.length > 0;
+  
+  const haveSpecificFilter = (key: string) => {
+    return selectedFilter === key
+  };
+  const toggleFilter = useStore(state => state.toggle);
+  const reset = useStore(state => state.reset);
+
+    const filteredItems = people.filter(
+        // If selected filters empty, show all...
+        item => selectedFilter === '' ||
+        // ...otherwise, item's tag must match selected filter
+        item.tag === selectedFilter);
+  const RenderFilters = (filters: string[]) => {
+    
+    const linkClass = 'no-underline border-b-2 border-b-[rgba(2,102,112,0)] hover:border-b-[rgba(2,102,112,1)] transition-all';
+  
+    const menu = <div>
+                    <ul>
+                        {filters.map(filter => {
+                            return (
+                                <li key={filter} className={`text-lg xl:text-sm font-semibold my-8 xl:my-4
+                                    ${!haveSpecificFilter(filter) ? 'text-bluegreen' : 'text-purple' }`}>
+                                    <a href="#" onClick={(e)=>{ toggleFilter(filter); e.preventDefault() }}
+                                        className='w-full flex items-center justify-between'>
+                                        <span className={!haveSpecificFilter(filter) ? linkClass : ''}>
+                                            {filter}
+                                        </span>
+                                        <svg viewBox="185.411 115.41 11 11" width="11" height="11"
+                                            className='flex-shrink-0 mx-6'
+                                            style={{visibility: !haveSpecificFilter(filter) ? 'hidden' : 'visible'}}>
+                                            <path
+                                                d="M 195.198 115.41 L 190.911 119.695 L 186.624 115.41 L 185.411 116.623 L 189.696 120.91 L 185.411 125.197 L 186.624 126.41 L 190.911 122.125 L 195.198 126.41 L 196.411 125.197 L 192.126 120.91 L 196.411 116.623 Z"
+                                                className="fill-purple"></path>
+                                        </svg>
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>;
+  
+    return(<div> 
+            {/* Tablet portrait+ */}
+            <div className="hidden lg:block">
+                <div className="mr-4 flex justify-between">
+                    <span>Filters</span>
+                    <a href="#" className="text-bluegreen" onClick={(e) =>{ reset(); e.preventDefault() }}  style={{visibility: !haveFilters ? 'hidden' : 'visible'}}>Clear</a>
+                </div>
+                {menu}
+            </div>
+        </div>);
+  
+  };
   return (
     <Layout>
-      <div>
-          <div
+      <div className='container mt-14 mb-24 xl:mt-16 px-4 xl:px-8'>
+          {/* <div
           className="container mt-14 mb-14 xl:mt-16 px-4 xl:px-8">
               <h2 className="text-2xl text-bluegreen font-semibold">About Our Community</h2>
               <DocumentRenderer document={page.values.document} renderers={DocRenderers(rendererOverrides)} componentBlocks={BlockRenderers()} />
-          </div>
-          <div className='container mt-14 mb-24 xl:mt-16 px-4 xl:px-8'>
-              <hr className='border-sorbet' />
-              <h2 className="text-xl text-coated font-semibold mt-14 mb-12">Our Community</h2>
+          </div> */}
+          <h2 className="text-xl text-coated font-semibold mt-14 mb-12">Our Community</h2>
+          {RenderFilters(['student', 'faculty', 'partner', 'staff' ])}
+          <div className="lg:ml-5 grid gap-6 xl:grid-cols-4 lg:grid-cols-2">
+              {/* <hr className='border-sorbet' /> */}
 
-              {people.map((person, i) => (
-                <div key={i} className='flex flex-col lg:flex-row mt-5'>
-                      <div className='flex-shrink-0'>
+              {filteredItems.map((person, i) => (
+                <div key={i} className='flex flex-col mt-5'>
+                      <div>
                           {person.image ?
                             <Image id={`thumb-${i}`} alt={`Thumbnail for person with name "${person.name}"`} imgId={person.image.publicId} width={300} transforms='f_auto,dpr_auto,c_thumb,g_face,ar_4:3' /> :
                             <svg viewBox="0 0 300 255" width="300" height="255" stroke="#000000" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#000000">
@@ -57,10 +172,12 @@ export default function Community({ page, people }: InferGetStaticPropsType<type
                             </svg>
                           }
                       </div>
-                      <div className='lg:ml-6 w-full lg:w-1/2 xl:w-1/3'>
+                      <div>
                           <h3 className='text-xl font-semibold text-coated'>{person.name}</h3>
                           <p className="mt-2 mb-8">{person.title}</p>
-                            {person.blurb && (
+                          <p className="text-purple">{person.tag}</p>
+                           
+                          {/* < {person.blurb && (
                               <p>
                                 <span className="text-coated font-semibold">
                                 What brings you here?
@@ -73,8 +190,7 @@ export default function Community({ page, people }: InferGetStaticPropsType<type
                               <p className="text-purple font-semibold">
                                 Engaged in remembrance of {person.remembrance}.
                               </p>
-                            )}
-                          {/* <DocumentRenderer document={person.content.document} renderers={renderers} componentBlocks={BlockRenderers} /> */}
+                            )}DocumentRenderer document={person.content.document} renderers={renderers} componentBlocks={BlockRenderers} /> */}
                       </div>
                   </div>
               ))}
@@ -99,6 +215,7 @@ export async function getStaticProps() {
     `people(orderBy: {name: asc}, where: { enabled: { equals: true }}) {
       name 
       title
+      tag
       blurb
       remembrance
       image {

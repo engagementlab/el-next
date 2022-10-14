1e9bd3b100dd569430c20ef9a236ee881ab62c52
Johnny Richardson <johnny.c.rich@gmail.com>
2022-10-14 18:14:17 -0400

    refactor(components): component export changes.

--- packages/components/src/index.tsx
+++
@@ -1,8 +0,0 @@
-export { BlockRenderers } from './blockRenderers';
-export { Button } from './button';
-export { DocRenderers } from './docRenderers';
-export { Favicon } from './favicon';
-export { FlexLayout } from './flexLayout';
-export { HeadingStyle } from './headingStyle';
-export { Image } from './image';
-export { Video } from './video';

29713ea372291614445d127dd44675cbe780eca8
Johnny Richardson <johnny.c.rich@gmail.com>
2022-10-19 11:08:46 -0400

    feat(functions): generate person key on PersonProfileCreate.

--- packages/functions/PersonProfileCreate/index.ts
+++ packages/functions/PersonProfileCreate/index.ts
@@ -11,6 +11,17 @@ cloudinary.config({
   api_secret: `${process.env.CLOUDINARY_SECRET}`,
   secure: true,
 });
+const CreateKey = (name: string) => {
+  let keyGen = name
+    .toLocaleLowerCase()
+    .replace(/[^\w ]/g, '')
+    .replace(/[^a-z+A-Z+0-9+]/gi, '-')
+    .replace(/-{2,}/g, '-');
+  if (keyGen.lastIndexOf('-') === keyGen.length - 1)
+    keyGen = keyGen.slice(0, keyGen.length - 1);
+
+  return keyGen;
+};
 
 const activityFunction: AzureFunction = async function (context: Context) {
   const body = new URLSearchParams(context.bindings.body.input);
@@ -40,10 +51,11 @@ const activityFunction: AzureFunction = async function (context: Context) {
 
     const personId = cuid();
     const text =
-      'INSERT INTO "Person" (id, "name", "image", "title", "blurb", "remembrance", "createdDate", "enabled") VALUES($1, $2, $3, $4, $5, $6, $7, $8)';
+      'INSERT INTO "Person" (id, "name", "key", "image", "title", "blurb", "remembrance", "createdDate", "enabled") VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
     const values = [
       personId,
       body.get('name'),
+      CreateKey(body.get('name')),
       { id: cuid(), _meta: response },
       body.get('title'),
       body.get('blurb'),

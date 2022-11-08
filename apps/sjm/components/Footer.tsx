615502555af7168c5aca4159498d28d5026cab64
Johnny Richardson <johnny.c.rich@gmail.com>
2022-11-08 18:13:31 -0500

    feat(sjm): generated nextjs app.

---
+++ apps/sjm/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Image from '@el-next/components/image';
+
+
+class Footer extends Component {
+  render() {
+    return (
+        <div>
+        </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ apps/sjm/pages/_app.tsx
@@ -0,0 +1,41 @@
+'use client'; 
+
+import '../styles/globals.scss'
+import React from 'react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head'
+import { Favicon } from '@el-next/components/favicon';
+
+import Header from '../components/Header'
+import Footer from '../components/Footer';
+import { AnimatePresence } from 'framer-motion';
+// import { AnimatePresence } from 'framer-motion';
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <div>
+        <Head>
+          <title>Transforming Narratives of Gun Violence</title>
+          {/* Block indexing on non-prod */}
+          {process.env.NODE_ENV !== 'production' && <meta name="robots" content="noindex" />}
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Transforming Narratives of Gun Violence is a multi-year initiative at Emerson in collaboration with the MGH Center for Gun Violence Prevention and the Louis D. Brown Peace Institute." />
+          <Favicon />
+        </Head>
+      </div>
+      <main className='w-full mb-24 font-sans'>
+      <Header />
+      <AnimatePresence
+                exitBeforeEnter
+                initial={false}
+                onExitComplete={() => window.scrollTo(0, 0)}>
+
+        <Component {...pageProps} />
+      </AnimatePresence>
+      <Footer />
+      </main>
+    </div>
+  )
+}
+
+export default App

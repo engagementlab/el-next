b136d26fcb4794e805b81479c1b43d92dd55fe1c
Johnny Richardson <johnny.c.rich@gmail.com>
2022-11-17 16:58:05 -0500

    refactor(components): blockrenderers now returns a function so each app can provide app-globalized params to all blocks.

--- packages/components/dist/declarations/src/blockRenderers/index.d.ts
+++ packages/components/dist/declarations/src/blockRenderers/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-export declare const BlockRenderers: (imageOveride?: (props: any) => JSX.Element, peopleOveride?: (peopleProps: any) => JSX.Element) => {
+export declare const BlockRenderers: (buttonClass?: string) => (imageOveride?: (props: any) => JSX.Element, peopleOveride?: (peopleProps: any) => JSX.Element) => {
     image: (props: any) => JSX.Element;
     video: (props: any) => JSX.Element;
     button: (props: any) => JSX.Element;
--- packages/components/src/blockRenderers/index.tsx
+++ packages/components/src/blockRenderers/index.tsx
@@ -4,38 +4,41 @@ import Link from "next/link";
 import Image from "../image";
 import { Video } from "../video";
 
-export const BlockRenderers = (imageOveride?: { (props: any): JSX.Element }, peopleOveride?: { (peopleProps: any): JSX.Element }) => {
-  let blocks = {
-    image: (props: any) => {
-      return imageOveride ? imageOveride(props) : 
-        (
+export const BlockRenderers = (buttonClass?: string) => {
+
+  return (imageOveride?: { (props: any): JSX.Element }, peopleOveride?: { (peopleProps: any): JSX.Element }) => {
+    let blocks = {
+      image: (props: any) => {
+        return imageOveride ? imageOveride(props) : 
+          (
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <Image id={'img-' + props.image.publicId} alt={props.image.alt} imgId={props.image.publicId} aspectDefault={true} />
+              {/* <p>{props.image.data.caption}</p> */}
+            </div>
+          );
+      },
+      video: (props: any) => {
+        return <Video videoLabel={props.video.label} videoUrl={props.video.value} thumbUrl={props.video.thumb} />;
+      },
+      button: (props: any) => {
+        return (
+          <Link href={props.link.props.node.children[0].text} passHref>
+            <button
+              className={`block lg:inline-block transition-all uppercase whitespace-nowrap ${buttonClass || 'px-9 py-7 mt-4'}`}>
+              {props.label}
+            </button>
+          </Link>
+        );
+      },
+      associatedPeople: (props: any) => {
+        return peopleOveride ? peopleOveride(props) : (
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <Image id={'img-' + props.image.publicId} alt={props.image.alt} imgId={props.image.publicId} aspectDefault={true} />
+            <p>ppl</p>
             {/* <p>{props.image.data.caption}</p> */}
           </div>
         );
-    },
-    video: (props: any) => {
-      return <Video videoLabel={props.video.label} videoUrl={props.video.value} thumbUrl={props.video.thumb} />;
-    },
-    button: (props: any) => {
-      return (
-        <Link href={props.link.props.node.children[0].text} passHref>
-          <button
-            className='block lg:inline-block rounded-full px-9 py-7 mt-4 uppercase whitespace-nowrap bg-lynx text-bluegreen border-2 border-bluegreen transition-all duration-700 hover:bg-green-blue hover:text-lynx hover:border-green-blue hover:scale-105'>
-            {props.label}
-          </button>
-        </Link>
-      );
-    },
-    associatedPeople: (props: any) => {
-      return peopleOveride ? peopleOveride(props) : (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <p>ppl</p>
-          {/* <p>{props.image.data.caption}</p> */}
-        </div>
-      );
+      }
     }
-  }
-  return blocks;
+    return blocks;
+  };
 };

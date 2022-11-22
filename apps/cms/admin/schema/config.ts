a70214428b966cf73c894745f39d35a456d42781
Johnny Richardson <johnny.c.rich@gmail.com>
2022-11-22 17:24:48 -0500

    fix(cms): do not run FixButtons in common config export for doc fields for now.

--- apps/cms/admin/schema/config.ts
+++ apps/cms/admin/schema/config.ts
@@ -1,7 +1,7 @@
 import { BaseListTypeInfo } from '@keystone-6/core/types';
 import { DocumentFieldConfig } from '@keystone-6/fields-document';
 import { componentBlocks } from '../components/component-blocks';
-import { FixButtons } from './hooks';
+// import { FixButtons } from './hooks';
 
 const BaseDocConfig = (
   layouts?: number[][]
@@ -18,11 +18,12 @@ const BaseDocConfig = (
     ui: {
       views: './admin/components/component-blocks',
     },
-    hooks: {
-      resolveInput: ({ resolvedData }: any) => {
-        return FixButtons(resolvedData);
-      },
-    },
+    // hooks: {
+    //   resolveInput: ({ resolvedData }: any) => {
+    //     console.log(resolvedData);
+    //     return FixButtons(resolvedData);
+    //   },
+    // },
   };
 };
 

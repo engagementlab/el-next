92029d226470995084422bcc2e2bbebcc7bd90ee
Johnny Richardson <johnny.c.rich@gmail.com>
2022-12-07 12:42:05 -0500

    refactor(tngvi): home page layout changes,

--- apps/tngvi/pages/index.tsx
+++ apps/tngvi/pages/index.tsx
@@ -29,24 +29,31 @@ export default function Home({ homePage }: InferGetStaticPropsType<typeof getSta
   
   return (
     <Layout>
-          <div className='flex flex-col xl:flex-row'>
+      <div className='flex flex-col'>
 
         <div className='relative w-full mt-20 lg:max-h-screen overflow-clip'>
           <div className='flex flex-col items-center'>
-            <div className='w-2/3 lg:mt-6 max-w-md text-center z-10'>
-              <h2 className='sm:text-xl lg:text-2xl font-semibold'>Nearly every person in the U.S. will know someone who has been shot in their lifetime.</h2>
+            <div className='lg:mt-6 max-w-md text-center z-10'>
+              <h2 className='sm:text-xl lg:text-2xl font-semibold'>Nearly every person in the U.S. will know someone who
+                has been shot in their lifetime.</h2>
               <h1 className='text-xl lg:text-[2rem] font-bold text-purple mt-6'>Everyone has a story to tell.</h1>
               <Button link='/archive' label='Listen to our stories' />
             </div>
           </div>
         </div>
         <div className='w-full flex flex-col items-center -translate-y-10 md:-translate-y-20'>
-        <Image id='img-home' alt='' imgId='tngvi/homepage' width={2200} className='w-full' lazy={false} />
-        <p className='w-4/5 md:w-1/2 lg:w-full text-sm lg:text-lg my-8 text-bluegreen text-center quote-shadow font-semibold '>Through local and collaborative storytelling, we seek to inspire solutions and interrupt cycles of gun violence.</p>
+          {/* <Image id='img-home' alt='' imgId='tngvi/homepage' width={2200} className='w-full' lazy={false} /> */}
         </div>
-        </div>
-        <Video videoLabel='Transforming Narratives of Gun Violence' videoUrl='https://vimeo.com/manage/videos/654694654' thumbUrl='https://res.cloudinary.com/engagement-lab-home/image/upload/v1670264461/tngvi/8c1ec98fa1cde870f3512551b62184a3.jpg' />
-
+      </div>
+      <Video videoLabel='Transforming Narratives of Gun Violence' videoUrl='https://vimeo.com/654694654'
+        thumbUrl='https://res.cloudinary.com/engagement-lab-home/image/upload/v1670433897/tngvi/home-video-thumb.png' />
+      <div className='flex flex-col items-center w-full text-center'>
+        <h3 className='w-4/5 md:w-1/2 lg:w-full text-sm lg:text-2xl my-5 md:my-6 text-purple font-bold'>Transforming Narratives of Gun Violence</h3>
+        <p
+          className='w-4/5 md:w-1/2 lg:w-full text-sm lg:text-lg mb-6 md:my-6 xl:mb-10 text-bluegreen font-semibold '>
+          Through local and collaborative storytelling, we seek to inspire solutions and interrupt cycles of gun
+          violence.</p>
+      </div>
     </Layout>
   );
 }

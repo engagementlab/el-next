1f33f22230a51e7408018c01178f03e07ae9c82f
Johnny Richardson <johnny.c.rich@gmail.com>
2023-01-20 17:14:52 -0500

    build: fixes for GH test action. fix(scripts): fix test script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-echo '$AZURE_STORAGE_ACCOUNT'
-echo $STORAGE_ACCOUNT
+# Have to re-assign vars from shell env
+export AZURE_STORAGE_CONNECTION_STRING=$AZURE_STORAGE_CONNECTION_STRING
+
+echo "------- Upload app build to Azure. -------"
+# Upload app build to Azure
+az storage blob upload-batch --overwrite true --account-name $STORAGE_ACCOUNT -d '$web' -s "./apps/${APP_NAME}/out"

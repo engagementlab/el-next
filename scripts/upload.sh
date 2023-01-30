5f3dd72f6c2644908d1ee0ae40f449b27f1c754a
Johnny Richardson <johnny.c.rich@gmail.com>
2023-01-30 17:42:16 -0500

    refactor(scripts): upload script changes.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,13 +2,12 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
 # Have to re-assign vars from shell env
-export AZURE_STORAGE_ACCOUNT=$AZURE_STORAGE_ACCOUNT
 export AZURE_STORAGE_CONNECTION_STRING=$AZURE_STORAGE_CONNECTION_STRING
 
 echo "------- Upload app build to Azure. -------"
 # Upload app build to Azure
-az storage blob upload-batch --overwrite true -d '$web' -s ./apps/tngvi/out
+az storage blob upload-batch --overwrite true --account-name $STORAGE_ACCOUNT -d '$web' -s "./apps/${APP_NAME}/out"
 
 echo "------- Clear CDN cache. -------"
 # Clear CDN cache
-az cdn endpoint purge -g web -n tngvi --content-paths '/*' --profile-name web-apps
+az cdn endpoint purge -g web -n $STORAGE_ACCOUNT --content-paths '/*' --profile-name web-apps

c7972a46fbcbf3405069aa5cb18b30cacbd57a40
Johnny Richardson <johnny.c.rich@gmail.com>
2023-03-01 13:06:11 -0500

    refactor(scripts): deploy changes.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /srv/apps/el-next/;
-git pull;
+cd /srv/apps/el-next/
+git pull
 
 # Remove exited containers
 docker ps -a -q -f status=exited | xargs --no-run-if-empty docker rm -v
@@ -15,29 +15,6 @@ docker images | awk '/ago/  { print $3}' | xargs --no-run-if-empty docker rmi
 # Remove dangling volumes
 docker volume ls -qf dangling=true | xargs --no-run-if-empty docker volume rm
 
-docker build -t el-next . --network=host;
-docker compose up -d;
-
-# Source/load nvm
-# [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh;
-
-# nvm use;
-# yarn;
-
-# # cd apps/cms;
-# # yarn;
-# # yarn export;
-
-# # cd ../tngvi;
-# # nvm use;
-# # yarn;
-# # yarn build;
-
-# # cd ../sjm;
-# # nvm use;
-# # yarn;
-# # yarn build;
-
-# pm2 restart 'app-tngvi'; 
-# pm2 restart 'app-sjm'; 
-
+# Build image and daemonize services
+yarn docker;
+docker compose up -d

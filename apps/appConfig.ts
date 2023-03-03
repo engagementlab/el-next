f1efd707d838ed7355724516edd42044a4268e8a
Johnny Richardson <johnny.c.rich@gmail.com>
2023-03-03 16:41:54 -0500

    refactor(cms): move appConfigMap to ./apps/appConfig for reusability as module.

---
+++ apps/appConfig.ts
@@ -0,0 +1,24 @@
+import { tngvi, sjm, elab } from './cms/admin/schema';
+
+type appConfigType = {
+  [key: string]: {
+    schema: object;
+    storageAccount: string;
+    apexUrl: string;
+  };
+};
+
+const appConfigMap: appConfigType = {
+  tngvi: {
+    schema: tngvi,
+    storageAccount: 'tngvi',
+    apexUrl: 'transformnarratives.org',
+  },
+  sjm: {
+    schema: sjm,
+    storageAccount: 'sjmsymposium',
+    apexUrl: 'sjmsymposium.org',
+  },
+  elab: { schema: elab, storageAccount: 'elabhome', apexUrl: '' },
+};
+export default appConfigMap;

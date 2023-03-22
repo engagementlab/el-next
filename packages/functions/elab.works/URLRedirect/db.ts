2d7117d0f64b7e1d16201aaf1bc3f037699edd65
Johnny Richardson <johnny.c.rich@gmail.com>
2023-03-22 12:54:54 -0400

    feat(functions): new subproject for elab.works cloud app.

---
+++ packages/functions/elab.works/URLRedirect/db.ts
@@ -0,0 +1,30 @@
+/**
+ * Engagement Lab URL Shortener
+ * Developed by Engagement Lab, 2020-23
+ * @author Johnny Richardson
+ *
+ */
+
+// Mongoose config w/ promise
+import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
+
+const dbAddress =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.MONGO_ADMIN_URI
+    : process.env.MONGO_CLOUD_ADMIN_URI;
+
+/**
+ * Create DB connection for admin database, which contains links collection.
+ * @name DB
+ * @module
+ */
+module.exports = () => {
+  try {
+    const conn = mongoose.createConnection(dbAddress);
+    return conn;
+  } catch (e) {
+    global.logger.error(e);
+    throw new Error(e);
+  }
+};
---
+++ packages/functions/elab.works/URLRedirect/index.ts
@@ -0,0 +1,64 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+
+import { GraphQLScalarType } from 'graphql';
+const { ApolloServer, gql } = require('apollo-server-azure-functions');
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('apollo-server-core');
+
+import path = require('path');
+// import RandExp = require('randexp');
+
+import { Analytics } from 'analytics';
+const googleAnalytics = require('@analytics/google-analytics').default;
+
+const connection = require('./db')();
+const Link = require('./Link')(connection);
+/**
+ * Analytics
+ */
+const analytics = Analytics({
+  app: 'Engagement Lab Website',
+  version: '2.5',
+  plugins: [
+    googleAnalytics({
+      trackingId: process.env.GA_TRACKING_ID,
+    }),
+  ],
+});
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  // Find original of by short url and increment clicks
+  const data = await Link.findOneAndUpdate(
+    {
+      shortUrl: req.query.key,
+    },
+    {
+      $inc: {
+        clicks: 1,
+      },
+    },
+    {
+      fields: 'originalUrl label',
+    }
+  ).exec();
+
+  // Track click in GA
+  // analytics.track('Shortener Click', {
+  //   label: data.label,
+  //   url: data.originalUrl,
+  // });
+
+  // Send user to URL
+
+  context.res = {
+    status: 302,
+    headers: { location: data.originalUrl },
+    body: null,
+  };
+};
+
+export default httpTrigger;

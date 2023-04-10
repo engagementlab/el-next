943b7aef0dedf1d4dad3f62c3a6cb7b2caf2ba40
Johnny Richardson <johnny.c.rich@gmail.com>
2023-04-10 17:40:34 -0400

    feat(tngvi): smooth scroll for all pages.

--- apps/tngvi/pages/_app.tsx
+++ apps/tngvi/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
-import Head from 'next/head'
+import Head from 'next/head';
 import { Favicon } from '@el-next/components/favicon';
 
-import Header from '../components/Header'
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { AnimatePresence } from 'framer-motion';
 // import { AnimatePresence } from 'framer-motion';
@@ -15,25 +15,33 @@ function App({ Component, pageProps }: AppProps) {
         <Head>
           <title>Transforming Narratives of Gun Violence</title>
           {/* Block indexing on non-prod */}
-          {process.env.NODE_ENV !== 'production' && <meta name="robots" content="noindex" />}
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="description" content="Transforming Narratives of Gun Violence is a multi-year initiative at Emerson in collaboration with the MGH Center for Gun Violence Prevention and the Louis D. Brown Peace Institute." />
+          {process.env.NODE_ENV !== 'production' && (
+            <meta name="robots" content="noindex" />
+          )}
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="Transforming Narratives of Gun Violence is a multi-year initiative at Emerson in collaboration with the MGH Center for Gun Violence Prevention and the Louis D. Brown Peace Institute."
+          />
           <Favicon />
         </Head>
       </div>
-      <main className='w-full mb-24 font-sans'>
-      <Header />
-      <AnimatePresence
-                exitBeforeEnter
-                initial={false}
-                onExitComplete={() => window.scrollTo(0, 0)}>
-
-        <Component {...pageProps} />
-      </AnimatePresence>
-      <Footer />
+      <main className="w-full mb-24 font-sans scroll-smooth">
+        <Header />
+        <AnimatePresence
+          exitBeforeEnter
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} />
+        </AnimatePresence>
+        <Footer />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

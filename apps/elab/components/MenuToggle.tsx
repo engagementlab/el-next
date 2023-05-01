30a9ee769a76c96b093efd139fc100afb3035e2a
Johnny Richardson <johnny.c.rich@gmail.com>
2023-05-01 17:50:50 -0400

    feat(apps): new app for elab home v3.x, WIP nav.

---
+++ apps/elab/components/MenuToggle.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { SVGMotionProps, motion } from 'framer-motion';
+
+const Path = (
+  props: JSX.IntrinsicAttributes &
+    SVGMotionProps<SVGPathElement> &
+    React.RefAttributes<SVGPathElement>
+) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="#fff"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+
+export const MenuToggle = ({ toggle, hover }: any, isHover: boolean) => (
+  <motion.button
+    onClick={toggle}
+    onMouseEnter={hover}
+    onMouseLeave={hover}
+    whileHover={{
+      scale: 1.2,
+      transition: { duration: 0.3 },
+    }}
+  >
+    {/* {isHover} */}
+    <svg
+      width="50"
+      height="50"
+      viewBox="0 0 50 50"
+      className="border-2 border-violet-700"
+    >
+      <motion.circle
+        cx="25"
+        cy="25"
+        r="20"
+        stroke="black"
+        fill="red"
+        animate={isHover ? 'hover' : 'default'}
+        variants={{
+          default: { fill: 'red' },
+          hover: { fill: 'black' },
+        }}
+      />
+      <Path
+        variants={{
+          closed: { d: 'M 2 2.5 L 20 2.5' },
+          open: { d: 'M 3 16.5 L 17 2.5' },
+        }}
+      />
+      <Path
+        d="M 2 9.423 L 20 9.423"
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 },
+        }}
+        transition={{ duration: 0.1 }}
+      />
+      <Path
+        variants={{
+          closed: { d: 'M 2 16.346 L 20 16.346' },
+          open: { d: 'M 3 2.5 L 17 16.346' },
+        }}
+      />
+    </svg>
+  </motion.button>
+);

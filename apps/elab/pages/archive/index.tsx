612c993c6bea84b3d4887ee87cd98ad04ccc6160
Johnny Richardson <johnny.c.rich@gmail.com>
2023-06-05 18:24:02 -0400

    feat(apps): elab app filtering WIP.

--- apps/elab/pages/archive/index.tsx
+++ apps/elab/pages/archive/index.tsx
@@ -17,11 +17,10 @@ interface ItemRendererProps<T> {
 }
 interface FilterState {
   currentFilters: never[];
-  filtersNavOpen: boolean;
-  filterGroupsClosed: never[];
+  // filtersNavOpen: boolean;
+  filterGroupOpen: string;
   toggle: (filter: any) => void;
-  toggleFilterGroupClosed: (filterKey: string) => void;
-  toggleFiltersOpen: (open: boolean) => void;
+  toggleFilterGroupOpen: (groupKey: string) => void;
   reset: () => void;
 }
 
@@ -30,15 +29,13 @@ type MediaItem = {
   key: string;
   shortDescription: string;
   filters: { key: string; name: string }[];
+  initiative: string;
   thumbnail: {
     publicId: string;
   };
 };
 
-const ItemRenderer = (props: {
-  item: MediaItem;
-  toggleFilter: (filter: string) => void;
-}) => {
+const ItemRenderer = (props: { item: MediaItem }) => {
   return (
     <motion.div
       animate={{ opacity: 1 }}
@@ -59,7 +56,7 @@ const ItemRenderer = (props: {
             // <ImagePlaceholder imageLabel="Media" width={335} height={200} />
           )} */}
         <h3 className="text-bluegreen text-xl font-semibold mt-4 hover:text-green-blue group-hover:text-green-blue">
-          ITEM
+          ITEM {props.item.initiative}
         </h3>
       </Link>
       <div className="mt-2 mb-20">
@@ -70,7 +67,7 @@ const ItemRenderer = (props: {
 };
 
 export default function MediaArchive({
-  filtersGrouped,
+  filters,
   mediaItems,
   error,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
@@ -85,8 +82,8 @@ export default function MediaArchive({
     subscribeWithSelector((set) => ({
       // If defined, pre-populate filter store
       currentFilters: preSelectedFilters || [],
-      filtersNavOpen: false as boolean,
-      filterGroupsClosed: [] as never[],
+      // filtersNavOpen: false as boolean,
+      filterGroupOpen: '',
       toggle: (filter: any) =>
         set((state) => {
           return state.currentFilters.includes(filter as never)
@@ -101,32 +98,15 @@ export default function MediaArchive({
                 currentFilters: [...state.currentFilters, filter as never],
               };
         }),
-      toggleFilterGroupClosed: (filterGroupKey: string) =>
+      toggleFilterGroupOpen: (filterGroupKey: string) =>
         set((state) => {
-          return state.filterGroupsClosed.includes(filterGroupKey as never)
-            ? {
-                ...state,
-                filterGroupsClosed: state.filterGroupsClosed.filter(
-                  (e) => e !== (filterGroupKey as never)
-                ),
-              }
-            : {
-                ...state,
-                filterGroupsClosed: [
-                  ...state.filterGroupsClosed,
-                  filterGroupKey as never,
-                ],
-              };
-        }),
-      toggleFiltersOpen: (open: boolean) =>
-        set((state) => {
-          document.body.style.overflow = open ? 'hidden' : 'visible';
-          if (open) window.scrollTo(0, 0);
           return {
             ...state,
-            filtersNavOpen: open,
+            filterGroupOpen:
+              state.filterGroupOpen === filterGroupKey ? '' : filterGroupKey,
           };
         }),
+
       reset: () =>
         set({
           currentFilters: [],
@@ -134,72 +114,95 @@ export default function MediaArchive({
     }))
   );
   useStore.subscribe(
-    (state) => state.currentFilters,
+    (state) => state.filterGroupOpen,
     (current) => {
       history.replaceState(
         {},
+        'Filtered Group',
+        `${location.pathname}?${current.toLocaleLowerCase()}`
+      );
+    }
+  );
+  useStore.subscribe(
+    (state) => state.currentFilters,
+    (current) => {
+      const { filterGroupOpen } = useStore((state) => state);
+      history.replaceState(
+        {},
         'Filtered Data',
-        `${location.pathname}?${current.join('/')}`
+        `${location.pathname}?${filterGroupOpen.toLowerCase()}/${current.join(
+          '/'
+        )}`
       );
     }
   );
 
-  const RenderFilters = (filters: { [x: string]: any[] }) => {
+  const RenderFilters = (filters: any[]) => {
     // Store get/set
-    const selectedFilters = useStore((state) => state.currentFilters);
-    const filtersOpen = useStore((state) => state.filtersNavOpen);
-    const filterGroupsClosed = useStore((state) => state.filterGroupsClosed);
-    const haveFilters = selectedFilters.length > 0;
+    const { currentFilters, filterGroupOpen, toggleFilterGroupOpen } = useStore(
+      (state) => state
+    );
+    const haveFilters = currentFilters.length > 0;
 
     const haveSpecificFilter = (key: string) => {
-      return _.values(selectedFilters).includes(key as never);
+      return _.values(currentFilters).includes(key as never);
     };
-    const haveGroupClosed = (key: string) => {
-      return filterGroupsClosed.includes(key as never);
+    const noGroupsOpen = () => {
+      return filterGroupOpen === '';
+    };
+    const haveGroupOpen = (key: string) => {
+      return filterGroupOpen === key;
     };
     const toggleFilter = useStore((state) => state.toggle);
-    const toggleFilterGroupOpen = useStore(
-      (state) => state.toggleFilterGroupClosed
-    );
-    const toggleFiltersOpen = useStore((state) => state.toggleFiltersOpen);
     const reset = useStore((state) => state.reset);
+
+    const filterGroups = [
+      { key: 'GunViolence', label: 'Gun Violence' },
+      { key: 'Climate', label: 'Climate' },
+    ];
 
     const menu = (
       <div>
-        {Object.keys(filters).map((key) => (
-          <div key={key}>
+        {filterGroups.map((group) => (
+          <div key={group.key}>
             <a
               href="#"
-              className="text-xl xl:text-base"
+              className={`text-xl ${
+                !noGroupsOpen() && !haveGroupOpen(group.key) && 'hidden'
+              } `}
               onClick={(e) => {
-                toggleFilterGroupOpen(key);
+                toggleFilterGroupOpen(group.key);
                 e.preventDefault();
               }}
             >
               <div className="mt-4 flex items-center flex-shrink-0 flex-grow-0 uppercase">
-                <svg
-                  height="10.0"
-                  width="14"
-                  className={`inline transition-transform ${
-                    haveGroupClosed(key) ? 'rotate-180' : ''
-                  }`}
-                >
-                  <polygon
-                    points="0,0 14,0 7.0,9.0"
-                    style={{ fill: '#8D33D2' }}
-                  ></polygon>
-                </svg>
                 <span className="ml-2 text-coated text-lg xl:text-sm font-semibold">
-                  {key}
+                  {group.label}
                 </span>
+                <svg
+                  viewBox="185.411 115.41 11 11"
+                  width="11"
+                  height="11"
+                  className="flex-shrink-0 mx-6"
+                  style={{
+                    visibility: !haveGroupOpen(group.key)
+                      ? 'hidden'
+                      : 'visible',
+                  }}
+                >
+                  <path
+                    d="M 195.198 115.41 L 190.911 119.695 L 186.624 115.41 L 185.411 116.623 L 189.696 120.91 L 185.411 125.197 L 186.624 126.41 L 190.911 122.125 L 195.198 126.41 L 196.411 125.197 L 192.126 120.91 L 196.411 116.623 Z"
+                    className="fill-purple"
+                  ></path>
+                </svg>
               </div>
             </a>
             <ul
-              className={`relative overflow-hidden transition-all ${
-                haveGroupClosed(key) ? 'max-h-0' : 'max-h-auto'
+              className={`relative transition-all ${
+                haveGroupOpen(group.key) ? 'block' : 'hidden'
               }`}
             >
-              {filters[key].map((filter) => {
+              {filters.map((filter) => {
                 return (
                   <li
                     key={filter.key}
@@ -255,83 +258,48 @@ export default function MediaArchive({
 
     return (
       <div>
-        {/* Tablet portrait+ */}
-        <div className="hidden lg:block">
-          <div className="mr-4 flex justify-between">
-            <span>Filters</span>
-            <a
-              href="#"
-              className="text-bluegreen"
-              onClick={(e) => {
-                reset();
-                e.preventDefault();
-              }}
-              style={{ visibility: !haveFilters ? 'hidden' : 'visible' }}
-            >
-              Clear
-            </a>
-          </div>
-          {menu}
-        </div>
-        {/* Mobile/tablet */}
-        <div
-          className={`lg:hidden block w-full absolute overflow-y-scroll top-0 left-0 h-full z-50 p-10 pt-20 bg-lynx
-                        transition-all ease-[cubic-bezier(0.075, 0.820, 0.165, 1.000)] duration-300 ${
-                          filtersOpen ? '' : '-translate-y-full'
-                        }`}
-        >
+        <div className="mr-4 flex justify-between">
+          <span>Filters</span>
           <a
-            className="uppercase w-full flex justify-end cursor-pointer text-bluegreen"
+            href="#"
+            className="text-bluegreen"
             onClick={(e) => {
-              toggleFiltersOpen(false);
+              reset();
               e.preventDefault();
             }}
+            style={{ visibility: !haveFilters ? 'hidden' : 'visible' }}
           >
-            <svg
-              viewBox="185.411 115.41 11 11"
-              width="11"
-              height="11"
-              className="flex-shrink-0 my-1.5 mx-3"
-            >
-              <path
-                d="M 195.198 115.41 L 190.911 119.695 L 186.624 115.41 L 185.411 116.623 L 189.696 120.91 L 185.411 125.197 L 186.624 126.41 L 190.911 122.125 L 195.198 126.41 L 196.411 125.197 L 192.126 120.91 L 196.411 116.623 Z"
-                className="fill-bluegreen"
-              ></path>
-            </svg>
-            Close Filters
+            Clear
           </a>
-          {menu}
-          <button
-            className="my-4 w-full rounded-full px-8 py-5 uppercase bg-purple text-white transition-all hover:opacity-75"
-            onClick={(e) => {
-              toggleFiltersOpen(false);
-            }}
-          >
-            Apply Filters
-          </button>
         </div>
+        {menu}
       </div>
     );
   };
   const FilteredItems = (props: { items: MediaItem[] | null }) => {
+    // Store get/set
+    const { filterGroupOpen } = useStore((state) => state);
+    const haveGroupOpen = (key: string) => {
+      return filterGroupOpen === key;
+    };
     let selectedFilters = useStore((state) => state.currentFilters);
 
     const haveFilters = selectedFilters.length > 0;
     const reset = useStore((state) => state.reset);
-    const toggleFilter = useStore((state) => state.toggle);
-    const toggleFiltersOpen = useStore((state) => state.toggleFiltersOpen);
 
     const filteredItems = props.items
-      ? props.items.filter(
-          // If selected filters empty, show all...
-          (item) =>
-            selectedFilters.length === 0 ||
-            // ...otherwise, item's filters must match ALL selected filters
-            _.every(
-              selectedFilters,
-              (r) => _.map(item.filters, 'key').indexOf(r) >= 0
-            )
-        )
+      ? props.items.filter((item) => {
+          // If selected groups empty, show all...
+          return (
+            filterGroupOpen === '' ||
+            // ...otherwise, item's filters must match group and ALL selected sub-filters
+            (item.initiative === filterGroupOpen &&
+              _.every(
+                selectedFilters,
+                (r) => _.map(item.filters, 'key').indexOf(r) >= 0
+              ))
+          );
+        })
       : [];
 
     const count = filteredItems.length;
@@ -339,46 +307,10 @@ export default function MediaArchive({
     const showing = `Showing ${count}`;
 
     return (
-      <div className="flex">
-        <div className="w-0 lg:w-1/5 flex-shrink-0 lg:border-r border-sorbet">
-          {RenderFilters(filtersGrouped)}
-        </div>
+      <div className="flex flex-col">
+        {RenderFilters(filters)}
 
         <div className="w-full">
-          {/* Mobile Filters/Clear button */}
-          <div className="lg:hidden inline-block w-full">
-            <button
-              className="rounded-full my-4 px-8 py-5 w-full uppercase bg-purple text-white transition-all hover:opacity-75"
-              onClick={(e) => {
-                toggleFiltersOpen(true);
-                e.preventDefault();
-              }}
-            >
-              Open Filters
-            </button>
-            <a
-              href="#"
-              className="py-2 text-bluegreen mt-2 mb-4 text-right text-lg font-semibold"
-              onClick={(e) => {
-                reset();
-                e.preventDefault();
-              }}
-              style={{ display: !haveFilters ? 'none' : 'block' }}
-            >
-              <svg
-                viewBox="185.411 115.41 11 11"
-                width="11"
-                height="11"
-                className="my-1.5 mx-3 inline-block"
-              >
-                <path
-                  d="M 195.198 115.41 L 190.911 119.695 L 186.624 115.41 L 185.411 116.623 L 189.696 120.91 L 185.411 125.197 L 186.624 126.41 L 190.911 122.125 L 195.198 126.41 L 196.411 125.197 L 192.126 120.91 L 196.411 116.623 Z"
-                  className="fill-bluegreen"
-                ></path>
-              </svg>
-              Clear Filters
-            </a>
-          </div>
           <span className="my-8 xl:my-4 uppercase w-full block text-right text-lg xl:text-sm font-semibold">
             {showing}
           </span>
@@ -391,11 +323,7 @@ export default function MediaArchive({
             ) : (
               <AnimatePresence>
                 {filteredItems.map((item, i: number) => (
-                  <ItemRenderer
-                    key={i}
-                    item={item}
-                    toggleFilter={toggleFilter}
-                  />
+                  <ItemRenderer key={i} item={item} />
                 ))}
               </AnimatePresence>
             )}
@@ -423,10 +351,9 @@ export async function getStaticProps() {
     `filters { 
             key
             name
-            initiative
         }`
   );
-
+  // console.log(filters);
   if (filters.error) {
     return {
       props: {
@@ -436,18 +363,6 @@ export async function getStaticProps() {
       },
     };
   }
-
-  // Group filters by initiative
-  const filtersGrouped = (filters as any[]).reduce(
-    (filterMemo, { initiative, key, name }) => {
-      (filterMemo[initiative] = filterMemo[initiative] || []).push({
-        key,
-        name,
-      });
-      return filterMemo;
-    },
-    {}
-  );
 
   const mediaItems = await Query(
     'mediaItems',
@@ -468,6 +383,7 @@ export async function getStaticProps() {
 				key
 				name
 			}
+			initiative
 			thumbnail { 
 				publicId
 			}
@@ -486,7 +402,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      filtersGrouped,
+      filters,
       mediaItems: mediaItems as MediaItem[],
     },
   };

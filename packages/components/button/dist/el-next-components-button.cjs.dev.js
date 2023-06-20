f65f4eb95ade6eb1ae947919101c539e8d23cf0c
Johnny Richardson <johnny.c.rich@gmail.com>
2023-06-20 17:06:23 -0400

    feat(packages): update button component to change anchor button style.

--- packages/components/button/dist/el-next-components-button.cjs.dev.js
+++ packages/components/button/dist/el-next-components-button.cjs.dev.js
@@ -32,13 +32,29 @@ var Button = function Button(_ref) {
     });
   };
   var classStr = "".concat(margin ? margin : "my-10", " hover:bg-[").concat(hoverColor ? hoverColor : '#ab45f8', "] hover:scale-105 inline-block rounded-full px-10 py-7 uppercase bg-purple text-white transition-all duration-700 ").concat(className);
+  var anchorClassStr = "block flex flex-col items-end overflow-hidden transition-all font-bold duration-700 group ".concat(className);
   if (anchorId) {
-    return /*#__PURE__*/jsxRuntime.jsx("button", {
-      className: classOverride || classStr,
+    return /*#__PURE__*/jsxRuntime.jsxs("button", {
+      className: classOverride || anchorClassStr,
       onClick: function onClick() {
         return scrollTo(anchorId);
       },
-      children: label
+      children: [/*#__PURE__*/jsxRuntime.jsxs("div", {
+        className: "flex items-center",
+        children: [/*#__PURE__*/jsxRuntime.jsx("svg", {
+          width: "17",
+          height: "17",
+          viewBox: "0 0 24 24",
+          className: "inline rotate-90 mr-2 transition-transform group-hover:rotate-180 ".concat(className),
+          children: /*#__PURE__*/jsxRuntime.jsx("path", {
+            d: "M24 22h-24l12-20z"
+          })
+        }), /*#__PURE__*/jsxRuntime.jsx("span", {
+          children: label
+        })]
+      }), /*#__PURE__*/jsxRuntime.jsx("hr", {
+        className: "transition-all w-full border-b-2 translate-x-[25px] group-hover:translate-x-0"
+      })]
     });
   }
   return /*#__PURE__*/jsxRuntime.jsx(Link__default["default"], {
--- packages/components/src/button/index.tsx
+++ packages/components/src/button/index.tsx
@@ -66,13 +66,25 @@ export const Button = ({
   const classStr = `${margin ? margin : `my-10`} hover:bg-[${
     hoverColor ? hoverColor : '#ab45f8'
   }] hover:scale-105 inline-block rounded-full px-10 py-7 uppercase bg-purple text-white transition-all duration-700 ${className}`;
+  const anchorClassStr = `block flex flex-col items-end overflow-hidden transition-all font-bold duration-700 group ${className}`;
   if (anchorId) {
     return (
       <button
-        className={classOverride || classStr}
+        className={classOverride || anchorClassStr}
         onClick={() => scrollTo(anchorId)}
       >
-        {label}
+        <div className="flex items-center">
+          <svg
+            width="17"
+            height="17"
+            viewBox="0 0 24 24"
+            className={`inline rotate-90 mr-2 transition-transform group-hover:rotate-180 ${className}`}
+          >
+            <path d="M24 22h-24l12-20z" />
+          </svg>
+          <span>{label}</span>
+        </div>
+        <hr className="transition-all w-full border-b-2 translate-x-[25px] group-hover:translate-x-0" />
       </button>
     );
   }

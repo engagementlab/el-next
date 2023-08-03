5b770931a9bbdc453342fb120477f26c92cc0603
Johnny Richardson <johnny.c.rich@gmail.com>
2023-08-03 16:46:32 -0400

    fix(apps) elab - fonts fix, use local file.

--- apps/elab/pages/_app.tsx
+++ apps/elab/pages/_app.tsx
@@ -1,16 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-import { Overpass } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const baseFount = Overpass({
-  subsets: ['latin'],
+const overpassFont = localFont({
+  src: '../public/overpass.ttf',
   variable: '--font-base',
+  //     style: 'normal',
+  //   },
+  //   {
+  //     path: './overpass-italic.ttf',
+  //     style: 'italic',
+  //   },
+  // ],
 });
 1;
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${baseFount.variable} font-sans`}>
+    <div className={`${overpassFont.variable} font-sans`}>
       <Component {...pageProps} />
     </div>
   );

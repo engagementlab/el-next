9ffaad1c6eb396aea3625f548b6728d75d4f95d3
Johnny Richardson <johnny.c.rich@gmail.com>
2023-08-07 18:32:45 -0400

    feat(apps): cms - elab schema changes.

--- apps/cms/admin/schema/elab/lists/initiativesLanding.ts
+++ apps/cms/admin/schema/elab/lists/initiativesLanding.ts
@@ -1,10 +1,11 @@
-import { list } from '@keystone-6/core';
+import { group, list } from '@keystone-6/core';
 import { text } from '@keystone-6/core/fields';
 import { document } from '@keystone-6/fields-document';
 import { allowAll } from '@keystone-6/core/access';
 import { Lists } from '.keystone/types';
 import { componentBlocks } from '../../../components/component-blocks';
 import { FixButtons } from '../../hooks';
+import { cloudinaryImage } from '../../../components/cloudinary';
 
 const InitiativesLanding: Lists.InitiativesLanding = list({
   access: allowAll,
@@ -22,10 +23,73 @@ const InitiativesLanding: Lists.InitiativesLanding = list({
         },
       },
     }),
-    description: text({
-      ui: {
-        displayMode: 'textarea',
+    ...group({
+      label: 'Intro Fields',
+      fields: {
+        intro: text({
+          ui: {
+            displayMode: 'textarea',
+          },
+        }),
+        introImage: cloudinaryImage({
+          //   label: 'Thumbnail/Header Image',
+          cloudinary: {
+            cloudName: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+            apiKey: `${process.env.CLOUDINARY_KEY}`,
+            apiSecret: `${process.env.CLOUDINARY_SECRET}`,
+            folder: 'elab-home-v3.x/landings',
+          },
+        }),
+        introImageAltText: text({
+          validation: {
+            isRequired: true,
+          },
+          label: 'Intro Image Alt Text ♿',
+          ui: { description: 'Describe appearance of Intro Image' },
+        }),
+        introImageCaption: text(),
       },
+    }),
+    tngvi: document({
+      formatting: true,
+      links: true,
+      layouts: [
+        [1, 1],
+        [2, 1],
+        [1, 2],
+      ],
+      ui: {
+        views: './admin/components/component-blocks',
+      },
+      label: 'TNGVI Summary',
+      componentBlocks,
+    }),
+    tn4ej: document({
+      formatting: true,
+      links: true,
+      layouts: [
+        [1, 1],
+        [2, 1],
+        [1, 2],
+      ],
+      ui: {
+        views: './admin/components/component-blocks',
+      },
+      label: 'TN4EJ Summary',
+      componentBlocks,
+    }),
+    everythingElse: document({
+      formatting: true,
+      links: true,
+      layouts: [
+        [1, 1],
+        [2, 1],
+        [1, 2],
+      ],
+      ui: {
+        views: './admin/components/component-blocks',
+      },
+      componentBlocks,
     }),
   },
   ui: {

49b54b8748806958bde2150513b879427872f717
Johnny Richardson <johnny.c.rich@gmail.com>
2023-08-30 10:44:26 -0400

    fix: cleanup script.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -10,7 +10,7 @@ docker ps -a -q -f status=exited | xargs --no-run-if-empty docker rm -v
 docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
 
 # Remove unused images
-docker images | awk '/ago/  { print $3}' | xargs --no-run-if-empty docker rmi
+# docker images | awk '/ago/  { print $3}' | xargs --no-run-if-empty docker rmi
 
 # Remove dangling volumes
 docker volume ls -qf dangling=true | xargs --no-run-if-empty docker volume rm

92f2d79ab7e6aab6ed9468542559ab2b46f32a96
Johnny Richardson <johnny.c.rich@gmail.com>
2023-09-20 11:19:30 -0400

    feat(apps): cms - new dev script, dependency and resolution updates.

---
+++ apps/cms/dev.ts
@@ -0,0 +1,44 @@
+(() => {
+  function exec(command: string) {
+    command = command.replace(/\\?\n/g, ''); // need to merge multi-line commands into one string
+
+    const spawn = require('child_process').spawn;
+    const childProcess = spawn(command, {
+      stdio: 'pipe',
+      shell: true,
+    });
+
+    return new Promise((resolve, reject) => {
+      let stdout = '';
+
+      childProcess.stdout.on('data', (data: { toString: () => any }) => {
+        console.log(`${data.toString()}`);
+      });
+
+      childProcess.on('error', function (error: { toString: () => any }) {
+        console.log(error.toString());
+
+        reject({ code: 1, error: error });
+      });
+
+      childProcess.on('close', function (code: number) {
+        console.log(`Command "${command}" exited`);
+        if (code > 0) {
+          reject('Command failed with code ' + code);
+        } else {
+          resolve({ code: code, data: stdout });
+        }
+      });
+    }).catch((err) => {
+      throw new Error(err);
+    });
+  }
+  try {
+    exec('rm -f admin/schema/index.ts');
+    exec('ln -s admin/schema/sjm/index.ts admin/schema/index.ts');
+    exec(`yarn keystone dev --app sjm`);
+    // exec(`cd apps/${response.value}; yarn dev`, AppType.FRONTEND);
+  } catch (error) {
+    console.error(error);
+  }
+})();

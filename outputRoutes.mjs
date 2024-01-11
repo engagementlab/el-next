ab8a446fa73a1d12310e4fa2b54016905e0cb3e2
Johnny Richardson <johnny.c.rich@gmail.com>
2024-01-11 12:32:41 -0500

    build: GH actions lighthouse actions change to use foo, outputRoutes scripts to now output URL paths for LH use.

--- outputRoutes.mjs
+++ outputRoutes.mjs
@@ -15,7 +15,7 @@ export default (async () => {
   const files = await recursiveReaddirFiles(
     `${process.cwd()}/apps/${argv.app}/out`,
     {
-      ignored: /(\.js|.css|.json|.ico|.png|.DS_Store|.woff2|favicon)$/,
+      ignored: /(\.js|.css|.json|.ico|.png|.DS_Store|.woff2|.ttf|favicon)$/,
       filter: (item) => {
         return item.path;
       },
@@ -26,7 +26,11 @@ export default (async () => {
       .replace(`${process.cwd()}/apps/${argv.app}/out`, 'http://localhost:8080')
       .replace('index.html', '')}"`;
   });
+  const outFile = `lhconfig.json`;
 
   if (fs.existsSync('.pa11yci')) fs.unlinkSync('.pa11yci');
+  if (fs.existsSync(outFile)) fs.unlinkSync(outFile);
+
   fs.writeFileSync('.pa11yci', `{"urls": [${urls}]}`);
+  fs.writeFileSync(outFile, `{"urlsJson": [${urls}]}`);
 })();

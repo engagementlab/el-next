7da54fa1eb9b46846cf55bbae00bbe95ddd2777f
Johnny Richardson <johnny.c.rich@gmail.com>
2024-03-06 16:18:33 -0500

    feat(apps): elab - new Tagline component for home to resolve re-rendering issues, recent/upcoming events cleanup.

--- apps/elab/pages/index.tsx
+++ apps/elab/pages/index.tsx
@@ -34,6 +34,7 @@ import {
   StudioProject,
   StudioUnion,
 } from '@/types';
+import { Tagline } from '@/components/Tagline';
 
 type Home = {
   newsItem?: News;
@@ -50,49 +51,9 @@ export default function Home({
   const [videoOverlayHeight, setVideoOverlayHeight] = useState(0);
   const [bgTargetElement, setBgVideo] = useState();
   const [showVideo, setShowVideo] = useState(false);
-  const [wordIndex, setWordIndex] = useState(0);
-  const [underlineOrder, setWordUnderlineOrder] = useState([0, 1, 2]);
 
-  const targetRef = useRef<HTMLDivElement>(null);
   const videoPlayerRef = useRef<HTMLVideoElement>(null);
   const bgVideoRef = useRef();
-
-  const wordTransition: Transition = {
-    y: {
-      type: 'tween',
-      duration: 1.8,
-      ease: cubicBezier(0.075, 0.82, 0.165, 1.0),
-    },
-    opacity: { duration: 1.1 },
-  };
-  const wordVariants: Variants = {
-    enter: {
-      y: -50,
-      opacity: 0,
-    },
-    center: {
-      y: 0,
-      opacity: 1,
-    },
-    exit: {
-      y: 50,
-      opacity: 0,
-    },
-  };
-  const definitionVariants: Variants = {
-    enter: {
-      y: 50,
-      opacity: 0,
-    },
-    center: {
-      y: 0,
-      opacity: 1,
-    },
-    exit: {
-      y: -50,
-      opacity: 0,
-    },
-  };
 
   const EnableVideoFallback = () => {
     // There is a problem with Vimeo, we will fall back to an image
@@ -118,48 +79,6 @@ export default function Home({
       });
     }
   };
-  const Definition = ({
-    word,
-    define,
-    index,
-    color,
-  }: {
-    word: string;
-    define: string;
-    index: number;
-    color: string;
-  }) => {
-    if (wordIndex === index)
-      return (
-        <motion.div
-          key={`word-${wordIndex}`}
-          variants={definitionVariants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            y: { duration: 1, ease: cubicBezier(0.075, 0.82, 0.165, 1.0) },
-            opacity: { duration: 0.8 },
-          }}
-          className="h-full flex items-center"
-        >
-          <div
-            className={`h-1/5 relative drop-shadow-[0px_0px_10px_#fff] ${color}`}
-          >
-            <p className="flex flex-row w-3/4 justify-start items-center">
-              {word}{' '}
-              <em className="text-sm font-semibold ml-3 animate-enterNoun">
-                noun
-              </em>
-            </p>
-            <p className="text-grey font-normal text-sm animate-unBlur">
-              {define}
-            </p>
-          </div>
-        </motion.div>
-      );
-    return <span key={`word-${wordIndex}`}></span>;
-  };
 
   const EventsRenderer = ({
     events,
@@ -170,11 +89,11 @@ export default function Home({
   }) => {
     return (
       <Gutter>
-        <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
+        <h2 className="text-2xl md:text-5xl text-grey font-bold my-7 lg:my-14">
           {upcoming ? 'Upcoming' : 'Recent'} Events
         </h2>
         <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">
-          {/* {events.map((item: Event, i: number) => {
+          {events.map((item: Event, i: number) => {
             return (
               <div key={`event-${i}`}>
                 <div className="flex-shrink">
@@ -184,14 +103,7 @@ export default function Home({
                 </div>
               </div>
             );
-          })} */}
-          <div key={`event-${0}`}>
-            <div className="flex-shrink">
-              <Link href={`/events/`} className="group">
-                <NewsEventRenderer item={events[0] as Item} i={0} />
-              </Link>
-            </div>
-          </div>
+          })}
         </div>
         <div className="flex md:flex-row justify-end lg:ml-5 mt-8 mb-16">
           <MoreButton label="See more events" link="/events" />
@@ -246,15 +158,6 @@ export default function Home({
       }
     }
   }, [bgVideoRef]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setWordIndex((wordIndex) => wrap(0, 3, wordIndex + 1));
-      setWordUnderlineOrder([...underlineOrder.slice(1, 3), underlineOrder[0]]);
-    }, 3500);
-    return () => clearInterval(interval);
-  }, [wordIndex]);
-
   return (
     <Layout
       ogTitle="Home"
@@ -330,151 +233,7 @@ export default function Home({
       fullBleed={true}
       error={error}
     >
-      <Gutter noMarginY={true}>
-        <>
-          {/* --- TAGLINE --- */}
-          <div id="tagline" className="flex static flex-col xl:pt-14">
-            <div className="flex justify-center text-2xl md:text-3xl xl:text-5xl font-extrabold mt-10 xl:mt-24">
-              <div className="text-slate w-3/4 drop-shadow-[0px_0px_15px_#fff]">
-                Advancing&nbsp;
-                <span className="inline-block text-purple">
-                  peace<span className="text-slate">,</span>
-                </span>
-                &nbsp;
-                <div className="inline-block">
-                  <span className="inline-block text-purple">equity</span>,
-                  &&nbsp;
-                </div>
-                <span className="text-purple">justice</span>
-                <br />
-                <div className="flex flex-col lg:flex-row h-20">
-                  through collaborative&nbsp;
-                  <div
-                    className={`flex flex-col font-extrabold ${
-                      wordIndex !== 0
-                        ? wordIndex === 1
-                          ? 'w-[4.1em]'
-                          : 'w-[3.1em]'
-                        : 'w-[5.5em]'
-                    }`}
-                  >
-                    <div
-                      className={`flex lg:inline-flex flex-col transition-all duration-700 overflow-hidden h-8 md:h-12`}
-                    >
-                      <div className="relative">
-                        <div className="text-yellow ">
-                          <AnimatePresence>
-                            {wordIndex === 0 && (
-                              <motion.div
-                                key={0}
-                                variants={wordVariants}
-                                initial="enter"
-                                animate="center"
-                                exit="exit"
-                                transition={wordTransition}
-                                className="absolute inline-block"
-                              >
-                                storytelling
-                              </motion.div>
-                            )}
-                          </AnimatePresence>
-                        </div>
-                        <div className="text-green">
-                          <AnimatePresence>
-                            {wordIndex === 1 && (
-                              <motion.div
-                                key={1}
-                                variants={wordVariants}
-                                initial="enter"
-                                animate="center"
-                                exit="exit"
-                                transition={wordTransition}
-                                className="absolute inline-block"
-                              >
-                                research
-                              </motion.div>
-                            )}
-                          </AnimatePresence>
-                        </div>
-                        <div className="text-red">
-                          <AnimatePresence>
-                            {wordIndex === 2 && (
-                              <motion.div
-                                key={2}
-                                variants={wordVariants}
-                                initial="enter"
-                                animate="center"
-                                exit="exit"
-                                transition={wordTransition}
-                                className="absolute inline-block"
-                              >
-                                design
-                              </motion.div>
-                            )}
-                          </AnimatePresence>
-                        </div>
-                      </div>
-                    </div>
-
-                    {underlineOrder.map((underlineI, i) => {
-                      const colors = ['bg-yellow', 'bg-green', 'bg-red'];
-                      return (
-                        <motion.hr
-                          key={`u-${underlineI}`}
-                          layout
-                          className={`h-1 border-none w-full ${
-                            colors[underlineI]
-                          } ${i === 0 ? 'animate-fill' : 'mt-1'}`}
-                        />
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <AnimatePresence>
-            {showVideo && (
-              <motion.div
-                key="definitions"
-                initial={{ opacity: 0, height: 0 }}
-                animate={{
-                  opacity: 1,
-                  height: vidH,
-                  transition: { duration: 0.5 },
-                }}
-                exit={{ opacity: 0 }}
-                className="flex justify-center xl:justify-end min-h-[10rem] max-h-screen"
-                ref={targetRef}
-              >
-                <div className="w-3/4 xl:w-1/3 h-1/3 relative text-xl font-extrabold">
-                  <Definition
-                    word="sto•ry•tell​•ing"
-                    define="The art of conveying a narrative or a sequence of events through
-                  spoken, written, or visual means."
-                    index={0}
-                    color="text-yellow"
-                  />
-                  <Definition
-                    word="re•search"
-                    define="The systematic investigation of the observable world"
-                    index={1}
-                    color="text-green"
-                  />
-                  <Definition
-                    word="de•sign"
-                    define="The intentional shaping of futures"
-                    index={2}
-                    color="text-red"
-                  />
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </>
-      </Gutter>
-
+      <Tagline showVideo={showVideo} vidH={vidH} />
       {/* ===== PAGE CONTENT ===== */}
       <AnimatePresence>
         {showVideo && (
@@ -492,105 +251,13 @@ export default function Home({
             {sections &&
               sectionOrder.map((key, index) => {
                 switch (key) {
-                  case 'events':
-                    if (
-                      sections.events.upcomingEvents &&
-                      sections.events.upcomingEvents.length > 0
-                    )
-                      return (
-                        <div key="upcoming-events">
-                          {index > 0 && <Divider />}
-                          {/* <EventsRenderer
-                            events={sections.events.upcomingEvents}
-                            upcoming={true}
-                          /> */}
-
-                          <Gutter>
-                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
-                              Upcoming Events
-                            </h2>
-                            <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">
-                              {sections.events.upcomingEvents.map(
-                                (item: Event, i: number) => {
-                                  return (
-                                    <div key={`event-${i}`}>
-                                      <div className="flex-shrink">
-                                        <Link
-                                          href={`/events/${item.key}`}
-                                          className="group"
-                                        >
-                                          <NewsEventRenderer
-                                            item={item as Item}
-                                            i={i}
-                                          />
-                                        </Link>
-                                      </div>
-                                    </div>
-                                  );
-                                }
-                              )}
-                            </div>
-                            <div className="flex md:flex-row justify-end lg:ml-5 mt-8 mb-16">
-                              <MoreButton
-                                label="See more events"
-                                link="/events"
-                              />
-                            </div>
-                          </Gutter>
-                        </div>
-                      );
-                    else if (
-                      sections.events.recentEvents &&
-                      sections?.events.recentEvents.length > 0
-                    )
-                      return (
-                        <div key="events">
-                          {index > 0 && <Divider />}
-                          {/* <EventsRenderer
-                            events={sections?.events.recentEvents}
-                          /> */}
-                          <Gutter>
-                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
-                              Recent Events
-                            </h2>
-                            <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">
-                              {sections.events.recentEvents.map(
-                                (item: Event, i: number) => {
-                                  return (
-                                    <div key={`event-${i}`}>
-                                      <div className="flex-shrink">
-                                        <Link
-                                          href={`/events/${item.key}`}
-                                          className="group"
-                                        >
-                                          <NewsEventRenderer
-                                            item={item as Item}
-                                            i={i}
-                                          />
-                                        </Link>
-                                      </div>
-                                    </div>
-                                  );
-                                }
-                              )}
-                            </div>
-                            <div className="flex md:flex-row justify-end lg:ml-5 mt-8 mb-16">
-                              <MoreButton
-                                label="See more events"
-                                link="/events"
-                              />
-                            </div>
-                          </Gutter>
-                        </div>
-                      );
-                    break;
                   case 'news':
                     if (sections.news && sections.news.length > 0)
                       return (
                         <div key="news">
                           {index > 0 && <Divider />}
                           <Gutter>
-                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
+                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-7 lg:my-14">
                               Recent News
                             </h2>
                             <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">
@@ -631,13 +298,31 @@ export default function Home({
                           </Gutter>
                         </div>
                       );
+                  case 'events':
+                    const upcoming =
+                      sections.events.upcomingEvents &&
+                      sections.events.upcomingEvents.length > 0;
+
+                    return (
+                      <div key="events">
+                        {index > 0 && <Divider />}
+                        <EventsRenderer
+                          events={
+                            upcoming
+                              ? sections.events.upcomingEvents
+                              : sections.events.recentEvents
+                          }
+                          upcoming={upcoming}
+                        />
+                      </div>
+                    );
                   case 'projects':
                     if (sections.projects && sections.projects.length > 0)
                       return (
                         <div key="projects">
                           {index > 0 && <Divider />}
                           <Gutter>
-                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
+                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-7 lg:my-14">
                               Featured Projects
                             </h2>
                             <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">
@@ -703,7 +388,7 @@ export default function Home({
                         <div key="studioss">
                           {index > 0 && <Divider />}
                           <Gutter>
-                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-14">
+                            <h2 className="text-2xl md:text-5xl text-grey font-bold my-7 lg:my-14">
                               Featured Studios
                             </h2>
                             <div className="lg:ml-5 grid xl:grid-cols-3 xl:gap-5 xl:gap-y-10 lg:grid-cols-2 lg:gap-2 text-grey">

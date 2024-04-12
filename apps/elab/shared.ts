917ce23ce3933f57d227b1036bfe8d6df363f690
Johnny Richardson <johnny.c.rich@gmail.com>
2024-04-12 17:46:04 -0400

    refactor(apps): elab - update StudioProjectsSort to handle research projects, refactor main studio projects index to query and handle initiative research projects and remove deprecated code.

--- apps/elab/shared.ts
+++ apps/elab/shared.ts
@@ -1,23 +1,34 @@
 import { HTMLProps } from 'react';
-import { Semester, StudioProject, Theming } from './types';
+import {
+  Project,
+  ResearchProject,
+  Semester,
+  StudioProject,
+  Theming,
+} from './types';
 
-export const StudioProjectsSort = (projects: StudioProject[]) => {
+export const StudioProjectsSort = (projects: Project[]) => {
   return projects.sort((a, b) => {
-    // Get year from semester key
-    const aYr = a.semester[0].key.match(/\d{4}/gm);
-    const bYr = b.semester[0].key.match(/\d{4}/gm);
-    // Get season name
-    const aSeason = a.semester[0].name.substring(
-      0,
-      a.semester[0].name.indexOf('-') - 1
-    );
-    const bSeason = b.semester[0].name.substring(
-      0,
-      b.semester[0].name.indexOf('-') - 1
-    );
+    // Get year from start year (research) or semester key
+    const aYr = a.startYear
+      ? [a.startYear]
+      : a.semester[0].key.match(/\d{4}/gm);
+    const bYr = b.startYear
+      ? [b.startYear]
+      : b.semester[0].key.match(/\d{4}/gm);
 
     if (aYr && bYr) {
-      if (aYr[0] === bYr[0]) {
+      // Compare seasons only for studio projects
+      if (aYr[0] === bYr[0] && !a.startYear && !b.startYear) {
+        // Get season name
+        const aSeason = a.semester[0].name.substring(
+          0,
+          a.semester[0].name.indexOf('-') - 1
+        );
+        const bSeason = b.semester[0].name.substring(
+          0,
+          b.semester[0].name.indexOf('-') - 1
+        );
         // If years AND season match maintain alphabetic order by name
         if (aSeason === bSeason) return a.name.localeCompare(b.name);
         // otherwise, if just years match, use fall vs spring
@@ -25,7 +36,7 @@ export const StudioProjectsSort = (projects: StudioProject[]) => {
       }
 
       // Just use year
-      return parseInt(bYr[0]) - parseInt(aYr[0]);
+      return parseInt(bYr[0].toString()) - parseInt(aYr[0].toString());
     }
 
     // Fallback to name alpha order

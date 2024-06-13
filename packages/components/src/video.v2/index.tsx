020bf02644d955e0c723ed48a4144043912242dc
Johnny Richardson <johnny.c.rich@gmail.com>
2024-06-13 16:00:25 -0400

    feat(packages): components - video player close button, if enabled.

--- packages/components/src/video.v2/index.tsx
+++ packages/components/src/video.v2/index.tsx
@@ -57,6 +57,7 @@ type VideoProps = {
   thumbPublicId?: string;
   isSlide?: boolean;
   noUi?: boolean;
+  showCloseButton?: boolean;
   play?: boolean;
   playing?: boolean;
   started?: () => void;
@@ -275,6 +276,7 @@ export const Video = ({
   isSlide,
   theme,
   noUi,
+  showCloseButton,
   play,
   started,
   ended,
@@ -513,6 +515,24 @@ export const Video = ({
             ref={wrapperRef}
             className="w-full h-full min-h-[inherit] overflow-y-hidden"
           >
+            {/* Close button */}
+            {showCloseButton && (
+              <button
+                onClick={() => toggleOpen(false)}
+                className={`transition-all duration-500 ${easing} absolute right-0 mr-2 mt-2 z-50 inline-block w-12 h-12 hover:scale-125`}
+              >
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  viewBox="0 0 24 24"
+                  className={theme.fill}
+                >
+                  <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
+                </svg>
+              </button>
+            )}
             {!error ? (
               <>
                 {buffer && (

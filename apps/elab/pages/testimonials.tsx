27346ec4aa81edef531ae0ce860c005f70a47cf2
Johnny Richardson <johnny.c.rich@gmail.com>
2024-08-21 18:12:22 -0400

    feat(apps): elab - WIP testimonials page.

---
+++ apps/elab/pages/testimonials.tsx
@@ -0,0 +1,87 @@
+import { InferGetStaticPropsType } from 'next';
+import { DocumentRenderer } from '@keystone-6/document-renderer';
+
+import { Query } from '@el-next/components';
+import { DefaultWhereCondition } from '@/types';
+import Layout from '../components/Layout';
+import { Blocks, Doc } from '@/components/Renderers';
+import { ReactElement } from 'react';
+
+export default function Testimonials({
+  items,
+  error,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout error={error} title="Testimonials">
+      <div className="container mt-14 mb-24 xl:mt-16 px-4 xl:px-8">
+        <h1 className="font-extrabold text-black">Testimonials</h1>
+
+        {items && (
+          <div className="grid mt-5 lg:grid-cols-2 lg:gap-10">
+            {items.map((item: { attribution: string; body: any }) => {
+              return (
+                <div className="">
+                  &ldquo;
+                  <DocumentRenderer
+                    document={item.body.document}
+                    componentBlocks={Blocks()}
+                    renderers={Doc({
+                      paragraph: (children: ReactElement[]) => {
+                        return (
+                          <>
+                            {children.map((child: ReactElement) => {
+                              return (
+                                <span className="my-4 whitespace-pre-wrap">
+                                  {child}
+                                  <br />
+                                  <br />
+                                </span>
+                              );
+                            })}
+                          </>
+                        );
+                      },
+                    })}
+                  />
+                  &rdquo; &mdash;&nbsp;{item.attribution}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
+}
+export async function getStaticProps() {
+  const testimonials = await Query(
+    'testimonials',
+    `testimonials(
+			${DefaultWhereCondition()},
+      orderBy: {
+          createdDate: desc
+      }		
+    ) { 
+        body {
+          document
+        }
+        attribution
+      }`
+  );
+
+  if (testimonials.error) {
+    return {
+      props: {
+        error: testimonials.error,
+        testimonials: null,
+      },
+    };
+  }
+
+  return {
+    props: {
+      items: testimonials,
+    },
+    revalidate: 1,
+  };
+}
